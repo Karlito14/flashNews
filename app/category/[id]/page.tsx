c1b8750d6c805f97525450d443a878018ed2dc3b
Karlito14 <leiroz26@hotmail.com>
2024-05-09 19:00:47 +0200

    ajout fake server

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function CatgeoryPage (props: { params: { id: keyof typeof CATEGO
     return (
         <div className='flex items-center space-x-4'>
             <Image src={categoryItem.src} alt={categoryItem.alt} width={40} height={40} />
-            <h1 className="font-bold capitalize text-3xl">{categoryItem.category}: dernières infos</h1>
+            <h1 className="font-bold capitalize text-3xl">{categoryItem.category} news</h1>
         </div>
     );
 };
